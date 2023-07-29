3927ce07f99b3326dc9f384f01be0e1ecf37bbcc
tobias <tobias.omondi@student.moringaschool.com>
2023-07-29 22:39:43 +0300

    my-assignment

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const lowercaseWord = word.toLowerCase();
 
-/* 
+
+  /* 
   Add your pseudocode here
 */
-
 /*
+
   Add written explanation of your solution here
+  intialize the pointers from left to right
+  convert our word to a lowercase 
+  moving pointers f the characters at the left and right pointers are equal,
 */
+// first use variable
+  let left = 0;
+  let right = lowercaseWord.length - 1;
+
+
+  while (left < right) {
+    if (lowercaseWord[left] !== lowercaseWord[right]) {
+      return false;
+    }
+
+    left++;
+    right--;
+    
+  }  return true;
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
